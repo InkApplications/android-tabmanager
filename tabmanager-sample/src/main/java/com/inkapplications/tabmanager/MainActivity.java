f00a280516cce9ce83e56ca4c78f3fd5ca81d619
Andrew Reitz <aj.reitz@gmail.com>
2014-03-22 23:06:21 -0500

    working on sample application

--- tabmanager-sample/src/main/java/com/inkapplications/tabmanager/MainActivity.java
+++ tabmanager-sample/src/main/java/com/inkapplications/tabmanager/MainActivity.java
@@ -1,9 +1,11 @@
 package com.inkapplications.tabmanager;
 
+import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,27 +14,20 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SupportTabManager tabManager = new SupportTabManager(this);
+
+        SupportTabContent tabContent1 = new SupportTabContent(
+                getString(R.string.tab1),
+                ExampleFragment.class
+
+        );
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+    static final class ExampleFragment extends Fragment {
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            return super.onCreateView(inflater, container, savedInstanceState);
         }
-        return super.onOptionsItemSelected(item);
     }
-
 }
